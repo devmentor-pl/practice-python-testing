b02cff03286fafca575a9db0da8cf3cd45ed6b66
Jakub Gromala <jakubgromala@MacBook-Pro-Jakub-2.local>
2025-04-07 15:49:52 +0200

    zad_05

--- 05/test_cart.py
+++ 05/test_cart.py
@@ -1,17 +1,32 @@
 import pytest
 from cart import Cart
 
-def test_add_product():
-    pass
+@pytest.fixture
+def cart():
+    return Cart()
 
-def test_remove_product():
-    pass
+def test_add_product(cart):
+    cart.add('apple', {'name': 'apple', 'price': 3.5})
+    assert 'apple' in cart.items()
 
-def test_total_price():
-    pass
+def test_remove_product(cart):
+    cart.add('banana', {'name': 'banana', 'price': 5})
+    cart.remove('banana')
+    assert 'banana' not in cart.items()
 
-def test_empty_cart_total():
-    pass
 
-def test_remove_nonexistent_product():
-    pass
+def test_total_price(cart):
+    cart.add('banana', {'name': 'banana', 'price': 5})
+    cart.add('apple', {'name': 'apple', 'price': 3.5})
+    assert cart.total() == 8.5
+
+
+def test_empty_cart_total(cart):
+    assert cart.total() == 0
+
+
+def test_remove_nonexistent_product(cart):
+    try:
+        assert cart.remove('banana')
+    except Exception as error:
+        print('Error:', error)
