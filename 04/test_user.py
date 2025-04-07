3191a46b87d5171c8d55c6e3292e58eb92260c2e
Jakub Gromala <jakubgromala@MacBook-Pro-Jakub-2.local>
2025-04-07 14:25:57 +0200

    zad_04

--- 04/test_user.py
+++ 04/test_user.py
@@ -2,5 +2,21 @@ import pytest
 from user import validate_user
 
 
-def valid_user_data():
-    pass
+@pytest.fixture
+def sample_data():
+    return {
+        'user': {'email': 'test@test.com',
+                 'age': 18,
+                 'name': 'Jakub'}
+    }
+
+
+def test_valid_user_data(sample_data):
+    email = sample_data['user']['email']
+    age = sample_data['user']['age']
+    name = sample_data['user']['name']
+    assert '@' in email
+    assert email.endswith('.pl') or email.endswith('.com')
+    assert isinstance(age, int)
+    assert age >= 18
+    assert name.isalpha()
