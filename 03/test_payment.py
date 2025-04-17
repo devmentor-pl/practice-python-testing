9784a3e673ae453bf4b9bb41bde219f3e38a660f
Jakub Gromala <jakubgromala@MacBook-Pro-Jakub-2.local>
2025-04-17 17:11:19 +0200

    update

--- 03/test_payment.py
+++ 03/test_payment.py
@@ -2,30 +2,33 @@ import unittest
 from unittest.mock import Mock
 from payment import send_payment
 
+
 class TestSendPayment(unittest.TestCase):
-   def test_successful_payment(self):
-      payment_data = {"amount": 100, "currency": "PLN"}
-      gateway = Mock()
-      gateway.pay.return_value = True
+    def test_successful_payment(self):
+        payment_data = {"amount": 100, "currency": "PLN"}
+        gateway = Mock()
+        gateway.pay.return_value = True
 
-      result = send_payment(payment_data, gateway)
-      self.assertTrue(result)
+        result = send_payment(payment_data, gateway)
+        self.assertTrue(result)
 
-   def test_failed_payment(self):
-      payment_data = {"amount": 100, "currency": "PLN"}
-      gateway = Mock()
-      gateway.pay.return_value = False
+    def test_failed_payment(self):
+        payment_data = {"amount": 100, "currency": "PLN"}
+        gateway = Mock()
+        gateway.pay.return_value = False
 
-      result = send_payment(payment_data, gateway)
-      self.assertFalse(result)
+        result = send_payment(payment_data, gateway)
+        self.assertFalse(result)
 
-   def test_payment_exception(self):
-      payment_data = {"amount": 100, "currency": "PLN"}
-      gateway = Mock()
-      gateway.pay.return_value = Exception('Error')
+    def test_payment_exception(self):
+        payment_data = {"amount": 100, "currency": "PLN"}
+        gateway = Mock()
+        gateway.pay.side_effect = Exception('Error')
+        gateway.pay.return_value = False
 
-      result = send_payment(payment_data, gateway)
-      self.assertFalse(result)
+        result = send_payment(payment_data, gateway)
+        self.assertFalse(result)
+      # sprawdzenie wyjątku  self.assertRaises
 
 
 if __name__ == '__main__':
